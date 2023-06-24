c6b2cb6a7abb9f387712983aeae81c13b9f65a2f
Asad <asadkhakimov05@gmail.com>
2023-06-25 03:31:11 +0600

    a

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+function Footer() {
+  return (
+    <div id="footer">
+      <div className="container">
+        <div className="footer"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
